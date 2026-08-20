87a962b62617566ad7fd26462d5d51aeebe39e07
Sanyam jain <dr.sanyamjain20@gmail.com>
2026-08-20 10:36:37 +0530

    Time: 4 ms (12.56%), Space: 23.9 MB (45.52%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -7,15 +7,14 @@ public:
         int k = 0;
         a.push_back(nums[k++]);
         b.push_back(nums[k++]);
-        int i = 0, j = 0;
         int la = a[0], lb = b[0];
         while (k < n) {
             if (la > lb) {
                 a.push_back(nums[k++]);
-                la = a[++i];
+                la = a.back();
             } else {
                 b.push_back(nums[k++]);
-                lb = b[++j];
+                lb = b.back();
             }
         }
         a.insert(a.end(), b.begin(), b.end());
